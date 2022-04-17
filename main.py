5513d520236a16282e31dc42e52fe4dfc801936e
harsh21p <harshadpachore21@gmail.com>
2022-04-17 12:46:20 +0530

    dropdown

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET', 'POST'])
 def home():
+   if request.method == 'POST':
+                name = request.form["dropdown1"]
+                
    return render_template('index.html')
 
 if __name__ == '__main__':
